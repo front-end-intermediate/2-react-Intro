388c07e9cddaf507605badbf377efb5775f76a75
Daniel Deverell <daniel.deverell@gmail.com>
2021-04-05 16:16:05 -0400

    added env variables

--- pirates/src/firebase.js
+++ pirates/src/firebase.js
@@ -1,16 +1,21 @@
 import firebase from "firebase/app";
 import "firebase/database";
 
+import Config from "./config.json";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC1yCzyIdwEDTha8YPLDqyTMxKPzIy0lrE",
-  authDomain: "pirates-31599.firebaseapp.com",
-  databaseURL: "https://pirates-31599-default-rtdb.firebaseio.com",
-  projectId: "pirates-31599",
-  storageBucket: "pirates-31599.appspot.com",
-  messagingSenderId: "79434369957",
-  appId: "1:79434369957:web:79f3f3b74964f5eb87f64c",
+  apiKey: Config.API_KEY,
+  databaseURL: Config.DATABASE_URL,
+  projectId: Config.PROJECT_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
 export default firebase;
+
+// https://stackoverflow.com/questions/43331011/firebase-app-named-default-already-exists-app-duplicate-app
+// https://developers.redhat.com/blog/2021/03/04/making-environment-variables-accessible-in-front-end-containers/
