42afee6686903813d3edb4cbf07f26b074b09985
Daniel Deverell <daniel.deverell@gmail.com>
2022-11-03 20:31:18 -0400

    done

--- pirates/src/components/Pirate.jsx
+++ pirates/src/components/Pirate.jsx
@@ -1,9 +1,27 @@
-export function Pirate({ name, children }) {
+import avatar from "../assets/img/avatar.png"
+import "../assets/css/Pirate.css"
+
+export function Pirate(props) {
+  const { name, year, weapon, vessel, desc } = props.pirate
+  const { tagline } = props
+
   return (
-    <p>
-      {name}, {children}
-    </p>
-  );
+    <section>
+      <summary>
+        <img src={avatar} alt="pirate" />
+        <h3>{name}</h3>
+        <ul>
+          <li>Died: {year}</li>
+          <li>Weapon: {weapon}</li>
+          <li>Vessel: {vessel}</li>
+        </ul>
+      </summary>
+      <article>
+        <h2>{tagline}</h2>
+        <p>{desc}</p>
+      </article>
+    </section>
+  )
 }
 
 // export default Pirate;
